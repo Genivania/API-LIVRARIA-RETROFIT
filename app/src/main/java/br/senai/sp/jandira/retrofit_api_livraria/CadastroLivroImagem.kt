31631a3c6a17406af3ad40e5a2cd4ef02887b86c
Genivania Macedo <109962733+Genivania@users.noreply.github.com>
2023-11-14 08:22:50 -0300

    ok

--- app/src/main/java/br/senai/sp/jandira/retrofit_api_livraria/CadastroLivroImagem.kt
+++ app/src/main/java/br/senai/sp/jandira/retrofit_api_livraria/CadastroLivroImagem.kt
@@ -1,10 +1,21 @@
 package br.senai.sp.jandira.retrofit_api_livraria
 
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 
 class CadastroLivroImagem : AppCompatActivity() {
+
+    //ATRIBUTOS DE MANIPULAÇÃO DE ENDEREÇOS DAS IMAGENS.
+    private var imageUriGRD: Uri? = null
+    private var imageUriPEQ: Uri? = null
+
+    //CONFIGURAÇÕES DO FIRE BASE
+    //DECLARAÇÃO DO STORAGE
+    //private lateinit var storageRef:
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.cadastro_livro_imagem)
@@ -12,5 +23,8 @@ class CadastroLivroImagem : AppCompatActivity() {
         //TESTE DE RECEBIMENTO DO JSON
         val bodyJson = intent.getStringExtra("boryJSON")
         Log.e("Teste-Json", bodyJson.toString())
+
+
+
     }
 }
